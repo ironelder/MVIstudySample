e99d3eb0f3af464f60de2f5c7db917c634378cd7
장진혁/Client개발팀/원스토어 <ironelder@onestorecorp.com>
2023-10-06 17:33:30 +0900

    [장진혁] 힐트 및 기본 동작 추가

---
+++ MVIstudySampleProject/app/src/main/java/com/example/mvistudysampleproject/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.mvistudysampleproject
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+}
